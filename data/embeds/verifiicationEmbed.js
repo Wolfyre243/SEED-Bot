7803616e7264a640ee4c86331a68875faadcdd48
raynnpjl <101748970+raynnpjl@users.noreply.github.com>
2025-04-27 01:00:13 +0800

    added verification embed

--- data/embeds/verifiicationEmbed.js
+++ data/embeds/verifiicationEmbed.js
@@ -1,1 +1,14 @@
-console.log("test")
+const verifyEmbed = new EmbedBuilder()
+  .setTitle("✅ VERIFY PANEL ✅")
+  .setDescription("This control panel is for Project: **Sedé**\nClick on the ***Verify***  button below to get your **VERIFIED** role")
+  .setImage("example")
+  .setColor("#10f500")
+  .setFooter({
+    text: "Sent by Sedé",
+    iconURL: "example",
+  })
+  .setTimestamp();
+
+module.exports = {
+    verifyEmbed
+}
