5a2e02203848d3280afd7c4404a20fec63626a9d
raynnpjl <101748970+raynnpjl@users.noreply.github.com>
2025-04-26 16:34:31 +0800

    +Verify Button

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const fs = require('node:fs');
 const path = require('node:path');
 
-const { Client, Collection, GatewayIntentBits } = require('discord.js');
+const { Client, Collection, Events, GatewayIntentBits, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 
 // const app = require('./app'); // Express server
 
@@ -14,3 +14,26 @@ const token = process.env.TOKEN;
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 //---------------------------------Main Script----------------------------------------
+
+client.on("ready", async () => {
+	console.log("The bot is ready");
+
+	const channel = await client.channels.fetch('1364637665571373256');
+	const messages = await channel.messages.fetch();
+
+	if (messages.size === 0) {
+		channel.send({
+			components: [
+				new ActionRowBuilder().addComponents(
+					new ButtonBuilder()
+					.setCustomId("open-modal")
+					.setLabel("Verify")
+					.setStyle(ButtonStyle.Success),
+				),
+			],
+		});
+	}
+});
+
+// Log in to Discord with your client's token
+client.login(token);
