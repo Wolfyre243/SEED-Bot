d364a58695002633d48cfd00d44a53cb71757942
Wolfyre243 <wolfyreblueflare@gmail.com>
2025-04-27 19:21:43 +0800

    [DEPLOYMENT] v0.1.0 bot ready for hosting

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const path = require('node:path');
 
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 
-const app = require('./app'); // Express server
+const app = require('./server/app'); // Express server
 
 const token = process.env.TOKEN;
 
