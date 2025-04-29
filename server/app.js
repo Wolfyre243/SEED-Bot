90763391128e5b2049241a33be53302556963302
Wolfyre243 <wolfyreblueflare@gmail.com>
2025-04-29 20:47:32 +0800

    [FEATURE] Added server entrypoint and main router (does not work to prevent breakage)

--- server/app.js
+++ server/app.js
@@ -1,6 +1,18 @@
+// Import dependencies
 const express = require('express');
+// // Import routers
+// const mainRouter = require('./routes/mainRoutes');
 
 const app = express();
+
+// // Middleware
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+
+// app.use('/api', mainRouter);
+
+// Webpage
+// app.use('/', express.static('public'));
 
 app.get('/',(req,res) => {
     res.status(200).send("SEDE reporting for duty!");
---
+++ server/routes/mainRouter.js
@@ -0,0 +1,7 @@
+// Import dependencies
+const express = require('express');
+
+const mainRouter = express.Router();
+
+// Export the main router
+module.exports = mainRouter;
